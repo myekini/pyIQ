60601f00d7457b4750e4e3e562c97119a975a425
myekini <myekini1@gmail.com>
2022-02-15 09:39:02 +0000

    upgrade

--- Python_Quiz/settings.py
+++ Python_Quiz/settings.py
@@ -135,3 +135,11 @@ MEDIA_ROOT = BASE_DIR/ "pyIQ/media_cdn/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+"""
+Django settings for learning_log project
+"""
+
+#my settings 
+LOGIN_URL = '/users/login'
--- pyIQ/views.py
+++ pyIQ/views.py
@@ -3,11 +3,42 @@ from django.urls import reverse
 from pyIQ.models import Question
 from pyIQ.forms import QuestionForm
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def index(request):
     """view first page"""
-    questions = Question.objects.all()
-    context = {'questions': questions}
-    return render(request, 'pyIQ/index.html', context)
+    if request.method =='POST':
+        print(request.POST)
+        questions = Question.objects.all()
+        score=0
+        wrong=0
+        correct=0
+        total=0
+        for q in questions:
+            total +=1
+            print(request.POST.get(q.question))
+            print(q.answer)
+            print()
+            if q.answer == request.POST.get(q.question):
+                score += 10
+                correct += 1
+            else:
+                wrong += 1
+        percent = score/(total*10) *100
+        context = {
+            'score':score,
+            'time': request.POST.get('timer'),
+            'correct':correct,
+            'wrong':wrong,
+            'percent':percent,
+            'total':total
+            }
+        return render(request,'pyIQ/result.html',context)
+
+    else:
+        questions = Question.objects.all()
+        context = {'questions': questions}
+        return render(request, 'pyIQ/index.html', context)
